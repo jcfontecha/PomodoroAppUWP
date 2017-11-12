4c132b80f660d629ac677a5dc7314b5d9ef70569
Juan Carlos Fontecha <jcfontecha@gmail.com>
2017-11-11 22:25:16 -0600

    Added basic progress indicator

---
+++ Pomodoro/Views/ProgressIndicatorUserControl.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Windows.Foundation;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Shapes;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Pomodoro.Views
+{
+    public sealed partial class ProgressIndicatorUserControl : UserControl
+    {
+        public float Radius { get; set; } = 80;
+        public float Increment { get; set; } = 0.07f;
+
+        public ProgressIndicatorUserControl()
+        {
+            this.InitializeComponent();
+
+            var circle = CreateCircle();
+
+            Canvas.Children.Add(circle);
+        }
+
+        private Shape CreateCircle()
+        {
+            var circle = new Polyline();
+            circle.Stroke = new SolidColorBrush(Windows.UI.Colors.Red);
+            circle.StrokeThickness = 10;
+            var points = new PointCollection();
+
+            for (double i = -Math.PI / 2; i <= (3 * Math.PI / 2 + Increment); i += Increment)
+            {
+                var point = GetCircleCoordinates(i);
+                points.Add(point);
+            }
+
+            circle.Points = points;
+
+            return circle;
+        }
+
+        private Point GetCircleCoordinates(double pi)
+        {
+            var point = new Point();
+            point.X = Radius * (Math.Cos(pi) + 1);
+            point.Y = Radius * (Math.Sin(pi) + 1);
+
+            return point;
+        }
+    }
+}
